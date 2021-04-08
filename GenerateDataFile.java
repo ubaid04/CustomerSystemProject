98128f3400d2969fc75d55fea258bff4135074eb
ubaidkhan2004 <335539185@gapps.yrdsb.ca>
2021-04-08 16:36:40 -0400

    commenting and formating

--- GenerateDataFile.java
+++ GenerateDataFile.java
@@ -1,27 +1,32 @@
+//Importing required java packages
 import java.util.Scanner;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 class GenerateDataFile {
     public static void main(String[] args) {
-        
+        //Initializing scanner
         Scanner reader = new Scanner(System.in);
-
+        
+        //Asking for file location
         System.out.println("How do you like to name the file:");
         String fileName = reader.nextLine();
         System.out.println("Where do you want to store the file:");
         String filePath = reader.nextLine();
 
         String fileLocation = filePath.concat("\\" + fileName);
-
+        
+        //setting necessary variables
         String idValue = "2004";
         String postalCode = "S4D";
         String firstName = "Ubaid";
         String lastName = "Khan";
         String city = "Markham";
         String creditCard = "349876456";
-
+        
+        //calling method
         createDataFile(idValue, postalCode, firstName, lastName, city, creditCard, fileLocation);
-
+        
+        //Letting user know that the file was created
         System.out.println("Data file created");
 
         reader.close();
@@ -30,6 +35,7 @@ class GenerateDataFile {
     public static void createDataFile(String idValue, String postalCode, String firstName, String lastName, String city, String creditCard, String fileLocation) {
 
         try{
+            //Create file with the given information
             BufferedWriter bw = new BufferedWriter(new FileWriter(fileLocation));
             bw.write("ID value,First Name,Last Name,City,Postal Code,Credit Card Number \n");
             bw.write(idValue + ",");
@@ -40,7 +46,8 @@ class GenerateDataFile {
             bw.write(creditCard + ",");
             bw.close();
         }
-
+        
+        //handle any errors
         catch(Exception ex) {
             ex.printStackTrace();
         }
--- ValidatePostalCodeTest.java
+++ ValidatePostalCodeTest.java
@@ -1,31 +1,39 @@
+//Import java packages
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 class ValidatePostalCodeTest {
     public static void main(String[] args){
+        //Setting necessary variables
         String postalCode = "T4L";
         boolean found = false;
+        
+        //calling method
         found = read(found,postalCode);
         // If it prints out true, it means it is working as the postal code above is in the csv file
         System.out.println(found); 
     }
-
+    
     public static boolean read(boolean found, String postalCode){
         BufferedReader objReader = null;
+        //file location 
         String fileName = "postal_codes.csv";
         String filePath = "C:\\Users\\Ubaid Khan\\Downloads\\";
 
         try{
             String currentLine;
+            //Setting what file to read
             objReader = new BufferedReader(new FileReader(filePath + fileName));
-
+            //reading file line by line
             while((currentLine = objReader.readLine()) != null) {
+                //if line starts with the given postal code
                 if(currentLine.startsWith(postalCode)) {
                     found = true;
                 }
             }
         }
-
+        
+        //handle any errors
         catch(IOException e) {
             e.printStackTrace();
         }
@@ -33,15 +41,16 @@ class ValidatePostalCodeTest {
         finally{
             try{
                 if(objReader != null) {
+                    //close reader
                     objReader.close();
                 }
             }
-            
+            //handle any errors
             catch(IOException ex) {
                 ex.printStackTrace();
             }
         }
-
+        //return if the postal code was real or not
         return found;
     }
 }
